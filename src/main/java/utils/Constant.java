90ced3050eeead31430c82e2d0e95ca866c317b6
Tristan Esteban <tristan.esteban@efrei.net>
2020-11-27 18:40:51 +0100

    commande sql

--- src/main/java/utils/Constant.java
+++ src/main/java/utils/Constant.java
@@ -4,12 +4,11 @@ import java.sql.Connection;
 
 public class Constant {
     public static final String JDBC_DRIVER = "org.h2.Driver";
-    public static final String DB_URL = "jdbc:h2:~/STEEDB2";
+    public static final String DB_URL = "jdbc:h2:~/ST2EEDB";
     public static final String DB_USER = "adm";
     public static final String DB_PWD = "adm";
     public static Connection conn = null;
 
     public static String LOGINSUCCESS = "You are now login";
     public static String LOGINFAIL = "Fail to login";
-
 }
